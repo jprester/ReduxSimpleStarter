7d5b8d93f7ff1f6657df1eb8d7a77cf2215087ee
Janko Prester <janko.prester@gmail.com>
2017-05-10 16:05:55 +0800

    test change

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,6 @@
 export default function() {
     return [
-        {title: "Javascript: The good parts", pages: 101},
+        {title: "Javascript: The good parts", pages: 121},
         {title: "Harry Potter", pages: 39},
         {title: "The Dark Tower", pages: 231},
         {title: "Eloquent Ruby", pages: 1}
